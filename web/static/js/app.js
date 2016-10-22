9598d3a54a27f4fba2d92d47f5f1d53c83c4b186
Marcus Bernales <mboperator@gmail.com>
2016-10-22 07:49:37 -0700

    Added eslint so atom would stop complaining

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,21 +1,17 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+// Phoenix' dependencies
+import '../../../deps/phoenix/priv/static/phoenix';
+import '../../../deps/phoenix_html/priv/static/phoenix_html';
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
+// Shiny new, hot React component
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
+class Root extends Component {
+  render() {
+    return (
+      <h1>omg so hot</h1>
+    );
+  }
+}
 
-// import socket from "./socket"
+render(<Root />, document.getElementById('root'));

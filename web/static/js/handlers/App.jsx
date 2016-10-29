24359f40708bd0a34540773b4d42b1c2e86ebd4c
Marcus Bernales <mboperator@gmail.com>
2016-10-29 13:56:51 -0700

    Added apollo to store

--- web/static/js/handlers/App.jsx
+++ web/static/js/handlers/App.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import { PageHeader } from 'rebass';
-import Root from '../components/Root';
+import { ApolloProvider } from 'react-apollo';
+import AppLayout from '../components/AppLayout';
+import client from '../utils/getClient';
+import store from '../utils/getStore';
 
 export default class App extends Component {
   render() {
     return (
-      <Root>
-        <PageHeader
-          description="worldwide"
-          heading="broadcast.love"
-        />
-      </Root>
+      <ApolloProvider store={store} client={client}>
+        <AppLayout>
+          <PageHeader
+            description="worldwide"
+            heading="broadcast.love"
+          />
+        </AppLayout>
+      </ApolloProvider>
     );
   }
 }
---
+++ web/static/js/utils/getClient.js
@@ -0,0 +1,5 @@
+import ApolloClient from 'apollo-client';
+const client = new ApolloClient();
+const getClient = () => client;
+
+export default getClient;

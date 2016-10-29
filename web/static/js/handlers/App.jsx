eed670ae5a192825afabb262ac9d1bfd309b2484
Marcus Bernales <mboperator@gmail.com>
2016-10-29 14:22:19 -0700

    Loading Contents from backend server

--- web/static/js/handlers/App.jsx
+++ web/static/js/handlers/App.jsx
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 import { PageHeader } from 'rebass';
 import { ApolloProvider } from 'react-apollo';
 import AppLayout from '../components/AppLayout';
+import ContentFeed from './ContentFeed';
 import client from '../utils/getClient';
 import store from '../utils/getStore';
 
 export default class App extends Component {
   render() {
     return (
-      <ApolloProvider store={store} client={client}>
+      <ApolloProvider store={store()} client={client()}>
         <AppLayout>
           <PageHeader
             description="worldwide"
             heading="broadcast.love"
           />
+          <ContentFeed />
         </AppLayout>
       </ApolloProvider>
     );
--- web/static/js/utils/getClient.js
+++ web/static/js/utils/getClient.js
@@ -1,5 +1,11 @@
-import ApolloClient from 'apollo-client';
-const client = new ApolloClient();
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://localhost:4000/api',
+  }),
+});
+
 const getClient = () => client;
 
 export default getClient;

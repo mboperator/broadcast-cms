6b703ab4d0e9c63b1e4e4ef5c1abfea0d1752347
Marcus Bernales <mboperator@gmail.com>
2016-11-06 12:14:02 -0800

    Starting image input component

---
+++ web/static/js/components/ImageInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from 'react-layout-components';
+
+const ImageInput = ({ value, onChange }) => (
+  <Box>
+    Hello
+  </Box>
+);
+
+export default ImageInput;
--- web/static/js/components/NewContentModal.jsx
+++ web/static/js/components/NewContentModal.jsx
@@ -4,6 +4,7 @@ import localModule from '../utils/localModule';
 import { compose, setPropTypes } from 'recompose';
 import { Box } from 'react-layout-components';
 import MarkdownInput from './MarkdownInput';
+import ImageInput from './ImageInput';
 
 import {
   Button,
@@ -15,52 +16,93 @@ import {
   PanelFooter,
 } from 'rebass';
 
-const NewContentModal = ({ actions, modal = {}, createContent }) => (
-  <div>
-    <ButtonCircle onClick={actions.open}>
-      Add
-    </ButtonCircle>
-    <Overlay
-      open={modal.open}
-      onDismiss={actions.close}
-    >
-      <Panel>
-        <PanelHeader>
-          Add Content
-        </PanelHeader>
-        <div>
-          <Input
-            label="Description"
-            name="description_input"
-            placeholder="Enter a description..."
-            onChange={({ target: { value } }) => {
-              actions.updateDescription(value);
-            }}
-            value={modal.description}
-            rounded
-            type="text"
-          />
-          <MarkdownInput
-            value={modal.data}
-            onChange={actions.updateData}
-          />
-        </div>
-        <PanelFooter>
-          <Box flex={2}>
-            <Button onClick={() => createContent({ ...modal })}>
-              Submit
-            </Button>
-          </Box>
-          <Box flex={4}>
-            <a href="#" onClick={actions.close}>
-              Cancel
-            </a>
-          </Box>
-        </PanelFooter>
-      </Panel>
-    </Overlay>
-  </div>
-);
+const inputForContentType = type => {
+  switch (type) {
+  case 'image':
+    return ImageInput;
+  case 'video':
+    return MarkdownInput;
+  case 'text':
+    return MarkdownInput;
+  default:
+    break;
+  }
+};
+
+const NewContentModal = ({ actions, modal = {}, createContent }) => {
+  const InputComponent = inputForContentType(modal.type);
+
+  return (
+    <div>
+      <ButtonCircle onClick={actions.open}>
+        Add
+      </ButtonCircle>
+      <Overlay
+        open={modal.open}
+        onDismiss={actions.close}
+      >
+        <Panel>
+          <PanelHeader>
+            Add Content
+          </PanelHeader>
+          <div>
+            <Box>
+              <Box>
+                <div
+                  onClick={() => actions.updateType('image')}
+                  style={{ padding: '10px' }}
+                >
+                  Image
+                </div>
+                <div
+                  onClick={() => actions.updateType('text')}
+                  style={{ padding: '10px' }}
+                >
+                  Text
+                </div>
+                <div
+                  onClick={() => actions.updateType('video')}
+                  style={{ padding: '10px' }}
+                >
+                  Video
+                </div>
+              </Box>
+            </Box>
+            <Box column>
+              <Input
+                label="Description"
+                name="description_input"
+                placeholder="Enter a description..."
+                onChange={({ target: { value } }) => {
+                  actions.updateDescription(value);
+                }}
+                value={modal.description}
+                rounded
+                type="text"
+              />
+              <InputComponent
+                value={modal.data}
+                onChange={actions.updateData}
+              />
+            </Box>
+          </div>
+          <PanelFooter>
+            <Box flex={2}>
+              <Button onClick={() => createContent({ ...modal })}>
+                Submit
+              </Button>
+            </Box>
+            <Box flex={4}>
+              <a href="#" onClick={actions.close}>
+                Cancel
+              </a>
+            </Box>
+          </PanelFooter>
+        </Panel>
+      </Overlay>
+    </div>
+  );
+};
 
 const modalModule = createModule({
   name: 'modal',
